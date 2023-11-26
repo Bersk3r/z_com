8cdb4d9a9eda7b57157557f57db67cd84f7defa5
Bersk3r <kiscwook@gmail.com>
2023-11-27 03:28:05 +0900

    middle save

--- z-com/src/app/(afterLogin)/layout.tsx
+++ z-com/src/app/(afterLogin)/layout.tsx
@@ -1,9 +1,17 @@
 import { ReactNode } from "react";
+import style from '@/app/(afterLogin)/layout.module.css';
 export default async function AfterLoginLayout({children}: {  children: ReactNode }) {
     return (
-        <div>
-            애프터로그인 레이아웃
-            {children}
+        <div className={style.container}>
+            <header className={style.leftSectionWrapper}>
+                <section className={style.leftSection}></section>
+            </header>
+            <div className={style.rightSectionWrapper}>
+                <div className={style.rightSectionInner}>
+                    <main className={style.main}>{children}</main>
+                    <section className={style.rightSection}></section>
+                </div>
+            </div>
         </div>
     );
 }
