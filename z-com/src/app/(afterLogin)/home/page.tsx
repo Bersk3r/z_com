0fdea8115339ee61af7b8bf5d8b3d678316eafd5
Bersk3r <kiscwook@gmail.com>
2023-11-29 03:10:40 +0900

    middle save

--- z-com/src/app/(afterLogin)/home/page.tsx
+++ z-com/src/app/(afterLogin)/home/page.tsx
@@ -1,7 +1,21 @@
+import style from './home.module.css';
+import TabProvider from "@/app/(afterLogin)/home/_component/tabProvider";
+import Tab from "@/app/(afterLogin)/home/_component/Tab";
 export default function Home() {
   return (
-    <main>
-      홈페이지
+    <main className={style.main}>
+      <TabProvider>
+          <Tab />
+          {/*<PostForm />*/}
+          {/*<Post />*/}
+          {/*<Post />*/}
+          {/*<Post />*/}
+          {/*<Post />*/}
+          {/*<Post />*/}
+          {/*<Post />*/}
+          {/*<Post />*/}
+          {/*<Post />*/}
+      </TabProvider>
     </main>
   )
 }
